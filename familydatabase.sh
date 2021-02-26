6b020f2b1cd3dbc673b702e4167c5c30b7e26f54
root user <root@domain.com>
2021-02-26 14:30:44 -0500

    First commit

---
+++ familydatabase.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "
+PROGRAM MENU
+1 - Display free disk space
+2 - Display free memory
+0 - exit program
+"
+echo -n "Enter selection: "
+read selection
+echo ""
+case $selection in
+	1 ) df -h ;;
+	2 ) free ;;
+	3 ) exit ;;
+	* ) echo "Please enter 1, 2, or 0"
+esac
